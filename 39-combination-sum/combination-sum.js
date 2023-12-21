88cf82a40490e0015eb721082524c44e87e229bd
Akash Pathak <43691089+akashpathak9587@users.noreply.github.com>
2023-12-21 22:22:17 +0530

    Time: 70 ms (47.30%) | Memory: 44.9 MB (83.04%) - LeetSync

---
+++ 39-combination-sum/combination-sum.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    candidates.sort((a,b)=>a-b);
+    let result = [];
+
+    function solve(start, current, target){
+        if(target == 0){
+            result.push([...current]);
+            return;
+        }
+
+        for(let i=start;i<candidates.length;i++){
+            if(candidates[i]>target) break;
+            current.push(candidates[i]);
+            solve(i, current, target-candidates[i])
+            current.pop();
+        }
+
+    }
+
+    solve(0, [], target);
+    return result;
+
+};
