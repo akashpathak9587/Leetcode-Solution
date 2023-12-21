c2f8da7384b1f698f69a99796e6fd13f593ac565
Akash Pathak <43691089+akashpathak9587@users.noreply.github.com>
2023-12-21 22:18:38 +0530

    Time: 55 ms (34.10%) | Memory: 42.1 MB (53.15%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let start = 0;
+    let end = nums.length-1;
+    let mid = Math.floor((start+end)/2);
+    if(target<nums[start]){
+        return 0;
+    } else{
+        while(start<=end){
+            let mid = Math.floor((start+end)/2);
+            if(nums[mid]>=target){
+                while(nums[mid]>=target){
+                    mid--;
+                }
+                return mid+1;
+            } if(nums[mid]<target){
+                start = mid+1;
+            } else{
+                end = mid-1;
+            }
+        }
+    }
+    if(start>=end && nums[nums.length-1]<target){
+        return nums.length;
+    }
+    
+};
