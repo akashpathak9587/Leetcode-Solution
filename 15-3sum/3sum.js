284ebd04ccc335ce1e8231a380824f740db6afba
Akash Pathak <43691089+akashpathak9587@users.noreply.github.com>
2024-01-04 15:09:57 +0530

    Time: 158 ms (51.02%) | Memory: 59.2 MB (39.63%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -9,18 +9,17 @@ var threeSum = function(nums) {
     let result = [];
     while(i<n-2){
         if(i==0 || (i>0 && nums[i]!=nums[i-1])){
-            let target = -nums[i];
             let left = i+1;
             let right = n-1;
             while(left<right){
-                let sum = nums[left] + nums[right];
-                if(target == sum){
+                let sum = nums[left] + nums[right]+nums[i];
+                if(sum == 0){
                     result.push([nums[i], nums[left], nums[right]]);
                     while(left<right && nums[left]==nums[left+1]) left++;
                     while(left<right && nums[right]==nums[right-1]) right--;
                     left++;
                     right--;
-                } else if(sum<target){
+                } else if(sum<0){
                     left++;
                 } else{
                     right--;
