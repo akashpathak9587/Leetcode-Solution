8c1af3fadfb0e8e71c00c310632d24b1c816dec4
Akash Pathak <43691089+akashpathak9587@users.noreply.github.com>
2024-08-05 21:34:22 +0530

    Time: 96 ms (42.33%) | Memory: 49.5 MB (72.74%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,16 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let n = nums.length;
-    let i=0;
-    while(i<n-1){
-        let j=i+1;
-        while(j<n){
-            if(nums[i]+nums[j]==target){
-                return [i,j];
+    for(let i=0;i<nums.length;i++){
+        for(let j=i+1;j<nums.length;j++){
+            if (nums[i]+nums[j]===target){
+                return [i, j];
             }
-            j++;
         }
-        i++;
     }
 };
